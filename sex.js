c2735b24775ecb31970bf59ba99e54139c9b8318
Flor <113700652+florxdd@users.noreply.github.com>
2023-03-12 18:49:14 +0300

    Update sex.js

--- sex.js
+++ sex.js
@@ -1,1 +1,10 @@
-sex baba
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('sex')
+    .setDescription('Flor anneni yalıyor'),
+    async execute(interaction, client) {
+        await interaction.reply({ content: 'Pepe' });
+    }
+}
